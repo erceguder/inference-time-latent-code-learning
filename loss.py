83ae4bdd19fbfc710d8798d2db1b8e3deeb088fb
Erce Güder <erce_guder@icloud.com>
2023-06-02 23:34:10 +0300

    fix style loss

--- loss.py
+++ loss.py
@@ -12,7 +12,6 @@ def d_logistic_loss(real_pred, fake_pred):
 def g_nonsaturating_loss(fake_pred):
     return F.softplus(-fake_pred).mean()
 
-
 def subnetworks(vgg, max_layers=5):
     subnetworks = []
     layers = []
@@ -33,23 +32,24 @@ def subnetworks(vgg, max_layers=5):
 def gramian_matrix(subnetworks, img, max_layers=5):
     assert img.ndim == 4 and img.shape[0] == 1, "Provide one image only."
 
-    feature_maps = []
+    layer_acts = [net(img) for net in subnetworks]
 
-    # img should be normalized with
-    #    mean: 0.485, 0.456, 0.406
-    #    std: 0.229, 0.224, 0.225
+    # convert the feature maps of each layer to gramian matrices
+    # https://www.cv-foundation.org/openaccess/content_cvpr_2016/papers/Gatys_Image_Style_Transfer_CVPR_2016_paper.pdf
 
-    for net in subnetworks:
-        feature_maps.append(net(img))
-
-    # convert the feature maps to gramian matrices
-    # https://en.wikipedia.org/wiki/Gram_matrix
+    # In the style transfer paper above, the squared error term between grams is divided by 4 * R_l^2 * C_l^2 (Eq. 4),
+    # similar to the code provided by the authors, **contrary to what's stated in the paper.**
     gramians = []
 
-    for map_ in feature_maps:
-        # reshape to (#feature maps, H*W)
+    for map_ in layer_acts:
+        # reshape to (C, H*W)
         map_ = map_.reshape(map_.shape[1], -1)
+
+        # R_l & C_l in the paper
+        R_l, C_l = map_.shape
+
         gramian = torch.matmul(map_, map_.transpose(0, 1))
+        gramian = gramian.div(2 * R_l * C_l)
 
         gramians.append(gramian)
 
@@ -65,6 +65,6 @@ def style_loss(subnetworks, real_img, fake_img):
     loss = 0.0
 
     for real_map, fake_map in zip(real_grams, fake_grams):
-        loss += F.mse_loss(real_map, fake_map)
+        loss += torch.sum((real_map - fake_map) ** 2)
 
     return loss

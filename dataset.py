95d967024d086c44a9d2f67c537ed313a6c7a414
Erce Güder <erce_guder@icloud.com>
2023-06-04 15:37:02 +0300

    convert to random indexing

--- dataset.py
+++ dataset.py
@@ -1,14 +1,18 @@
 from PIL import Image
+import torch
 import glob
 import os
 
 class Dataset:
-    def __init__(self, path, transforms):
+    def __init__(self, path, device, transforms):
         files = glob.glob(os.path.join(path, "*.png"))
+
         self.imgs = list()
 
         for file in files:
-            self.imgs.append(transforms(Image.open(file)))
+            self.imgs.append(transforms(Image.open(file).convert("RGB")))
+
+        self.imgs = torch.stack(self.imgs, dim=0).to(device)
 
     def __getitem__(self, idx):
         return self.imgs[idx]

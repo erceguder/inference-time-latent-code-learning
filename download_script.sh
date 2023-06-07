d416de0ae3dff676d0dc0bce3a0c949874f2174f
Erce Güder <erce_guder@icloud.com>
2023-06-07 22:27:19 +0300

    add download of test data

--- download_script.sh
+++ download_script.sh
@@ -3,3 +3,6 @@
 wget https://user.ceng.metu.edu.tr/~adhd/courses/550000.pt
 wget https://user.ceng.metu.edu.tr/~adhd/courses/babies_images.zip
 unzip babies_images.zip -d babies
+
+python -c "import gdown; url = 'https://drive.google.com/u/0/uc?id=1JmjKBq_wylJmpCQ2OWNMy211NFJhHHID'; output = 'babies_real_test.zip'; gdown.download(url, output, quiet=False)"
+unzip babies_real_test.zip -d babies_test

7b421029acb4739f602f35d095e2d5c9890f4e9f
Erce Güder <erce_guder@icloud.com>
2023-06-09 23:54:43 +0300

    update download script

--- download_script.sh
+++ download_script.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 
-# download the FFHQ checkpoint: https://github.com/rosinality/stylegan2-pytorch#pretrained-checkpoints
-# and target FFHQ-Babies dataset (10 images only)
-wget https://user.ceng.metu.edu.tr/~adhd/courses/550000.pt
+export KAGGLE_USERNAME="adnanharundoan" KAGGLE_KEY="51b9a167d974acb6bf619f1bd9e92ebc"
 
-wget https://user.ceng.metu.edu.tr/~adhd/courses/babies_images.zip
-wget https://user.ceng.metu.edu.tr/~adhd/courses/bitmoji_images.zip
-wget https://user.ceng.metu.edu.tr/~adhd/courses/bitmoji_test_images.zip
+python -c "import gdown; url = 'https://drive.google.com/u/0/uc?id=1PQutd-JboOCOZqmd95XWxWrO8gGEvRcO&export=download'; output = '550000.pt'; gdown.download(url, output, quiet=True)"
 
-unzip babies_images.zip -d babies
-unzip bitmoji_images.zip -d bitmoji
-unzip bitmoji_test_images.zip -d bitmoji_test
+python -c "import gdown; url = 'https://drive.google.com/u/0/uc?id=1Uu5y_y8Rjxbj2VEzvT3aBHyn4pltFgyX&export=download'; output = 'sunglasses_real_test.zip'; gdown.download(url, output, quiet=True)"
 
-# download FFHQ-Babies full, to calculate FID scores
-python -c "import gdown; url = 'https://drive.google.com/u/0/uc?id=1JmjKBq_wylJmpCQ2OWNMy211NFJhHHID'; output = 'babies_real_test.zip'; gdown.download(url, output, quiet=False)"
-unzip babies_real_test.zip -d babies_test
+python -c "import gdown; url = 'https://drive.google.com/u/0/uc?id=1JmjKBq_wylJmpCQ2OWNMy211NFJhHHID'; output = 'babies_real_test.zip'; gdown.download(url, output, quiet=True)"
+
+kaggle datasets download -d mostafamozafari/bitmoji-faces &
+wget --quiet http://mmlab.ie.cuhk.edu.hk/archive/sketchdatabase/CUHK/training_88/Cropped_Images/CUHK_training_cropped_sketches.zip &
+
+wait < <(jobs -p)
+
+mkdir -p data
+unzip -q babies_real_test.zip -d babies_test &
+unzip -q bitmoji-faces.zip -d data &
+unzip -q sunglasses_real_test.zip -d sunglasses_test &
+unzip -q CUHK_training_cropped_sketches.zip -d data &
+
+wait < <(jobs -p)
+rm -rf *.zip
+
+mv data/sketches data/sketches_test
+mv babies_test/images data/babies_test
+mv sunglasses_test/images data/sunglasses_test
+mv data/BitmojiDataset/images data/bitmoji_test
+rmdir babies_test sunglasses_test
+rm -r data/BitmojiDataset
 
 # Resize images to output resolution
-python resize.py babies_test
-python resize.py bitmoji --extension jpg
-python resize.py bitmoji_test --extension jpg
+python resize.py data/babies_test &
+python resize.py data/sketches_test --extension jpg &
+python resize.py data/bitmoji_test --extension jpg & 
+python resize.py data/sunglasses_test &
+
+wait < <(jobs -p)
+
+for dataset in {babies,bitmoji,sketches,sunglasses}
+do
+	mkdir -p data/$dataset
+	samples=$(ls data/"$dataset"_test/*)
+	samples=$(shuf -e $samples)
+	samples=( $samples )
+	samples=${samples[@]:0:10}
+	mv $samples data/$dataset
+done

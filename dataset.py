e88cc9191806d9f42ec483841939a8114676aff5
Erce Güder <erce_guder@icloud.com>
2023-06-10 17:52:41 +0300

    update utilities

--- dataset.py
+++ dataset.py
@@ -4,15 +4,15 @@ import glob
 import os
 
 class Dataset:
-    def __init__(self, path, extension, transforms):
-        files = glob.glob(os.path.join(path, f"*.{extension}"))
+    def __init__(self, root, path, extension, transforms, n_imgs=10):
+        files = glob.glob(os.path.join(root, path, f"*.{extension}"))
         self.imgs = list()
 
         for file in files:
             self.imgs.append(transforms(Image.open(file).convert("RGB")))
 
         self.imgs = torch.stack(self.imgs, dim=0)
-        self.x = torch.randn(10, 14, 1024)
+        self.x = torch.randn(n_imgs, 14, 1024)
 
     def __getitem__(self, idx):
         return self.imgs[idx], self.x[idx]
--- utils.py
+++ utils.py
@@ -3,14 +3,27 @@ import numpy as np
 import shutil
 import os
 import subprocess
+
 from prettytable import PrettyTable
 from PIL import Image
+from model import Generator
+from latent_learner import LatentLearner
+
+def disable_grad(model):
+    for _, param in model.named_parameters():
+        param.requires_grad = False
 
 @torch.no_grad()
-def save_samples(experiment, iter_, x, generator, latent_learner):
-    if experiment == 0 and iter_ == 0:
-        shutil.rmtree("samples")
-        os.makedirs("samples")
+def save_samples(metadata, samples_root, experiment, x, generator, latent_learner):
+    path = os.path.join(samples_root, metadata["path"])
+
+    if experiment == 0:
+        os.makedirs(samples_root, exist_ok=True)
+        try:
+            os.makedirs(path)
+        except:
+            shutil.rmtree(path)
+            os.makedirs(path)
 
     generator.eval()
     latent_learner.eval()
@@ -21,7 +34,7 @@ def save_samples(experiment, iter_, x, generator, latent_learner):
     imgs = tensor_to_img(samples)
 
     for i, im in enumerate(imgs):
-        im.save(f"samples/samples_{experiment}_{iter_}_{i}.png",)
+        im.save(os.path.join(path, f"samples_{experiment}_{i}.png"))
 
 def tensor_to_img(samples):
     imgs = list()
@@ -37,16 +50,36 @@ def tensor_to_img(samples):
 
     return imgs
 
-def disable_grad(model):
-    for _, param in model.named_parameters():
-        param.requires_grad = False
+def save_models(metadata, ckpts_root, experiment, generator, latent_learner, noise):
+    path = os.path.join(ckpts_root, metadata["path"])
 
-def save(exp, generator, latent_learner, noise):
-    os.makedirs("ckpts", exist_ok=True)
+    if experiment == 0:
+        os.makedirs(ckpts_root, exist_ok=True)
+        try:
+            os.makedirs(path)
+        except:
+            shutil.rmtree(path)
+            os.makedirs(path)
 
-    torch.save(noise, f"ckpts/noise_{exp}.pt")
-    torch.save(latent_learner.state_dict(), f"ckpts/latent_learner_{exp}.pt")
-    torch.save(generator.state_dict(), f"ckpts/generator_{exp}.pt")
+    torch.save(generator.state_dict(), os.path.join(path, f"generator_{experiment}.pt"))
+    torch.save(latent_learner.state_dict(), os.path.join(path, f"latent_learner_{experiment}.pt"))
+    torch.save(noise, os.path.join(path, f"noise_{experiment}.pt"))
+
+def load_models(metadata, ckpts_root, experiment, resolution, w_size, n_mlp, device):
+    path = os.path.join(ckpts_root, metadata["path"])
+
+    generator = Generator(size=resolution, style_dim=w_size, n_mlp=n_mlp).to(device)
+    generator.load_state_dict(torch.load(os.path.join(path, f"generator_{experiment}.pt"), map_location=device))
+
+    latent_learner = LatentLearner().to(device)
+    latent_learner.load_state_dict(torch.load(os.path.join(path, f"latent_learner_{experiment}.pt"), map_location=device))
+
+    noise = torch.load(os.path.join(path, f"noise_{experiment}.pt"), map_location=device)
+
+    generator.eval()
+    latent_learner.eval()
+
+    return generator, latent_learner, noise
 
 # taken from https://stackoverflow.com/a/65583584
 def image_grid(imgs, rows, cols):
@@ -61,21 +94,59 @@ def image_grid(imgs, rows, cols):
 
     return grid
 
-def metrics(samples, test_imgs, device):
-    args = ["python",  "-m", "pytorch_fid", test_imgs, samples]
+def FID(samples_root, data_root, metadata, device):
+    our_fid_scores = list()
+    org_fid_scores = list()
 
-    if device==torch.device("cuda"):
-        args.extend(["--device", "cuda:0"])
+    table = PrettyTable()
+    table.field_names = ["Method"] + list(metadata.keys())
 
-    p = subprocess.Popen(
-        args,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
+    for dset in metadata.keys():
+        args = ["python",  "-m", "pytorch_fid"]
+        args.append(os.path.join(data_root, metadata[dset]["test_path"]))
+        args.append(os.path.join(samples_root, metadata[dset]["path"]))
 
-    out = p.communicate()[0].decode("ascii")
-    fid = out.split("FID")[1]
-    fid = fid.lstrip(':').lstrip(' ').rstrip('\n')
-    fid = float(fid)
+        if device == "cuda":
+            args.extend(["--device", "cuda:0"])
 
-    return fid
+        p = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        try:
+            out = p.communicate()[0].decode("ascii").split("FID")[1]
+            fid = out.lstrip(':').lstrip(' ').rstrip('\n')
+        except:
+            fid = float('nan')
+
+        our_fid_scores.append(float(fid))
+
+        ##### Original implementation scores
+
+        args = ["python",  "-m", "pytorch_fid"]
+        args.append(os.path.join(data_root, metadata[dset]["test_path"]))
+        args.append(os.path.join("./genda_samples", metadata[dset]["path"]))
+
+        if device == "cuda":
+            args.extend(["--device", "cuda:0"])
+
+        p = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        try:
+            out = p.communicate()[0].decode("ascii").split("FID")[1]
+            fid = out.lstrip(':').lstrip(' ').rstrip('\n')
+        except:
+            fid = float('nan')
+
+        org_fid_scores.append(float(fid))
+
+    table.add_row(["Ours"] + our_fid_scores, divider=True)
+    table.add_row(["Original"] + org_fid_scores, divider=True)
+
+    return table
